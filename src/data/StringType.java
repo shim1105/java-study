9983c7bed3df1466cfddd5e2e2b1391123701a30
Administrator <Administrator@USER-PC>
2019-02-13 11:58:38 +0900

    StringType 수정

--- src/data/StringType.java
+++ src/data/StringType.java
@@ -4,5 +4,7 @@ public class StringType {
 	public static void main(String[] args) {
 		String str= "123";
 		System.out.println(str+123);
+		String str2= new String("123");
+		System.out.println(str.equals(str2));
 	}
 }

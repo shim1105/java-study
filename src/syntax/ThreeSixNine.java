32d574e7d2608890b352853c08ca346118508f13
shim1105 <sim439411@gmail.com>
2019-02-14 08:48:46 +0900

    다시 수정

--- src/syntax/ThreeSixNine.java
+++ src/syntax/ThreeSixNine.java
@@ -4,7 +4,7 @@ public class ThreeSixNine {
 	public static void main(String[] args) {
 		for(int i=1;i<30;i++) {
 			int mod= i%10;
-			if(mod%3 == 0  || mod == 0) {
+			if(mod%3 == 0  && mod != 0) {
 				System.out.print("짝 , ");
 			}else {
 				System.out.print(i+", ");

aa3bc345b0972b975629db2cfd96a12370c365d9
Administrator <Administrator@USER-PC>
2019-02-13 11:35:15 +0900

    첫 자바프로젝트 깃 연동

---
+++ src/data/Type01.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Type01 {
+	public static void main(String[] args) {
+		//문자
+		char c ='a';
+		
+		//숫자 정수
+		byte b=127;
+		b=-128;
+		short sh=123;
+		int i =12313123;
+		long l =12312312312312313l;
+		//숫자 실수
+		float f=1.12f;
+		double db=1.234;
+		
+		//논리형
+		boolean bl =true;
+		bl=false;
+	}
+}

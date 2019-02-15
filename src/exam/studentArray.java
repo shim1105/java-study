67cf2bf6d16332460bb3b29e6fba5741df2a698d
shim1105 <sim439411@gmail.com>
2019-02-15 15:24:07 +0900

    11

---
+++ src/exam/studentArray.java
@@ -0,0 +1,20 @@
+package exam;
+
+class Student{
+	String name;
+	int grade;
+	String shooclName;
+}
+
+public class studentArray {
+	public static void main(String[] arge) {
+		Student[] students= new Student[10];// students 만 메모리생성
+		for(int i=0;i<students.length;i++) {//배열의 값이 null로 되는것을
+											//배열을 다시 만듬
+			students[i]=new Student();   // students[i]의 메모리를 생성
+					
+		}
+		System.out.println(students[9].grade);
+		
+	}
+}
